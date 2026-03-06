49d866fbe874f3834570df916fbbbb8288ea7488
Zenkuro <52635649+Zenkuro01@users.noreply.github.com>
2026-03-06 11:25:30 +0200

    refactor Book constructor to ES6 class

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-}
+class Book {
+  constructor(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
 
-Book.prototype.toggleRead = function () {
-  this.isRead = !this.isRead;
-};
+  toggleRead() {
+    this.isRead = !this.isRead;
+  }
+}
 
 function addBookToLibrary(title, author, pages, isRead) {
   const book = new Book(title, author, pages, isRead);
